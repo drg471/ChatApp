589b1949c5486ab155bb078b16ff6f4eec8bfcbb
drg <113433396+drg471@users.noreply.github.com>
2025-07-03 08:53:33 +0200

    Add files via upload

---
+++ Services/ChatService.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Services;
+
+use App\Exceptions\ChatServiceException;
+use App\Repositories\ChatRepository;
+use Exception;
+use Illuminate\Support\Facades\Crypt;
+
+class ChatService
+{
+    public function __construct(protected ChatRepository $chatRepository) {}
+
+    public function getAllChats($user)
+    {
+        try {
+            $chats = $this->chatRepository->getAllChatsByUser($user);
+
+            return $chats;
+        } catch (Exception $e) {
+            throw new ChatServiceException("Error obtener los chats del usuario.", 0, $e);
+        }
+    }
+
+    public function getChatMessages($chatId, $user)
+    {
+        try {
+            $chatMessagesEncrypted = $this->chatRepository->getChatMessagesByChatId($chatId);
+
+            $chatMessagesDecrypted = [];
+
+            foreach($chatMessagesEncrypted as $chatMessageEncrypted){
+                $messageDecrypted = Crypt::decryptString($chatMessageEncrypted['message']);
+                
+                $chatMessagesDecrypted [] = [
+                    'id' => $chatMessageEncrypted['id'],
+                    'chat_id' => $chatMessageEncrypted['chat_id'],
+                    'sender_id' => $chatMessageEncrypted['sender_id'],
+                    'message' => $messageDecrypted,
+                    'created_at' => $chatMessageEncrypted['created_at'],
+                ];
+            }
+
+            return $chatMessagesDecrypted;
+        } catch (Exception $e) {
+            throw new ChatServiceException("Error obtener los mensajes del chat {$chatId}: ", 0, $e);
+        }
+    }
+
+    public function saveSentMessage($chatId, $userSender, $message)
+    {
+        try {
+            $messageEncrypted = Crypt::encryptString($message);
+
+            $saveMessage = $this->chatRepository->saveMessage($chatId, $userSender, $messageEncrypted);
+
+            return $saveMessage;
+        } catch (Exception $e) {
+            throw new ChatServiceException("Error guardar el mensaje enviado del chat {$chatId}: ", 0, $e);
+        }
+    }
+
+    public function sendChatInvitation($user, $emailInvitation)
+    {
+        try {
+            $chatData  = $this->chatRepository->sendChatInvitationByEmail($user, $emailInvitation);
+
+            return $chatData;
+        } catch (Exception $e) {
+            throw new ChatServiceException("Error enviar invitación a chat al usuario {$emailInvitation}: ", 0, $e);
+        }
+    }
+
+    public function setChatMessagesRead($messagesUnreadIds, $userId)
+    {
+        try {
+            $messagesMarkAsRead = $this->chatRepository->setChatMessagesReadByMessagesUnreadIds($messagesUnreadIds, $userId);
+
+            return $messagesMarkAsRead;
+        } catch (Exception $e) {
+            throw new ChatServiceException("Error marcar mensajes como leídos con id's {$messagesUnreadIds}: ", 0, $e);
+        }
+    }
+
+    public function deleteChat($chatId)
+    {
+        try {
+            $deletedChat  = $this->chatRepository->delete($chatId);
+
+            return $deletedChat;
+        } catch (Exception $e) {
+            throw new ChatServiceException("Error al eliminar el chat {$chatId}: ", 0, $e);
+        }
+    }
+}
